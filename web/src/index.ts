48ebbbc08368990de6ac86b6b8ef5a28f87950cf
Gill Chan <jiahao0408@gmail.com>
2022-06-20 22:50:18 +0800

    fix: import

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,6 @@
 import launcher from "./launcher";
 import register from "./register";
+import invoker, { jsbridge } from "./invoker";
+import callback_wrapper from "./callback_wrapper";
 
-export { launcher, register };
+export { launcher, register, invoker, jsbridge, callback_wrapper };
